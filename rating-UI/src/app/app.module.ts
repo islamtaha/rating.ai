56be4fdfd9b4b7fe5a608da76ee7da89b9b62089
ELERAKY Islam Ext O-EG/HRCS <itaha48@yahoo.com>
2019-07-17 15:29:32 +0200

    added home, search, ratings, menu-bar and footer

--- rating-UI/src/app/app.module.ts
+++ rating-UI/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { RatingsComponent } from './ratings/ratings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    FooterComponent,
+    HomeComponent,
+    SearchComponent,
+    RatingsComponent
   ],
   imports: [
     BrowserModule,
